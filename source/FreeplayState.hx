1d5ccedb88efb2d6ba5fe0d3e241d58719d97fcc
SINIX-porter <sla950989@gmail.com>
2024-07-29 20:03:31 -0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -207,9 +207,11 @@ class FreeplayState extends MusicBeatState
 		text.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, RIGHT);
 		text.scrollFactor.set();
 		add(text);
+		
 		#if android
-		addVirtualPad(LEFT_FULL, A_B_C)
+		addVirtualPad(LEFT_FULL, A_B_C);
 		#end
+			
 		super.create();
 	}
 
