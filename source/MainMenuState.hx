e797741610d53eb59437f461d29b587615c7b8ed
SINIX-porter <sla950989@gmail.com>
2024-07-29 19:53:13 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -211,7 +211,7 @@ class MainMenuState extends MusicBeatState
                 #if android
 		addVirtualPad(UP_DOWN, A_B)
 		#end
-		super.create()
+		super.create();
 	}
 
 	var selectedSomethin:Bool = false;
