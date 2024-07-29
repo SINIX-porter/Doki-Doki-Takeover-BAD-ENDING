a2a0130d550c03be610f6d6d1dd0f09244fae407
SINIX-porter <sla950989@gmail.com>
2024-07-29 20:27:50 -0300

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -175,7 +175,7 @@ class PauseSubState extends MusicBeatSubstate
 		changeSelection();
 
                 #if android
-		addVirtualPad(UP_DOWN, A_B)
+		addVirtualPad(UP_DOWN, A_B);
 		#end
 		
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
